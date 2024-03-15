91935efe50c5bb323102ef2a94b9ead7b01b773d
berrymorr <berrymorr@gmail.com>
2024-03-15 15:55:21 +0000

    always restart container

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 docker container rm -f YTcastsBot
 docker image rm -f ytcasts
 docker build -t ytcasts . && \
-docker run --detach --name YTcastsBot ytcasts:latest
+docker run --restart=always --detach --name YTcastsBot ytcasts:latest
